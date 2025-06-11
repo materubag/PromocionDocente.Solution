c0f0d98f4914aef49ab4fabcf795c562b9c0c3c8
Josue Guevara <el.jdragon@gmail.com>
2025-06-11 14:24:22 -0500

    Importe de obras a la Base de datos Local completo
    Se crearon varias Bibliotecas de clases con prefjo PromocionDocente. :
    -aplication
    -Domain
    -Infraestructure
    Dentro de ella se creó diferentes carpetas para alamcenar los metodos com
    import services, el modelo Obra, el controlado, el contexto de ambas bases de datos (Conexcion y definicion de columnas) (DAC y Promociondocente)  se define la ObraDAc que toma directamnte d ela  BD y de ahí la manda a la local, y por ultimo el servicio de ObraImportservice

---
+++ PromocionesDocente.Infraestructure/Contexts/PromocionDocenteDbContext .cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PromocionDocente.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromocionDocente.Infrastructure.Contexts
+{
+    public class PromocionDocenteDbContext : DbContext
+    {
+        public PromocionDocenteDbContext(DbContextOptions<PromocionDocenteDbContext> options) : base(options) { }
+
+        public DbSet<Obra> Obras { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Obra>(entity =>
+            {
+                entity.ToTable("OBRAS");
+
+                entity.HasKey(e => e.IdObra); // ⬅️ ESTO ES CLAVE
+
+                entity.Property(e => e.IdObra).HasColumnName("ID_OBRA");
+                entity.Property(e => e.CedulaDocente).HasColumnName("CED_DOC");
+                entity.Property(e => e.TipoObra).HasColumnName("TIPO_OBRA");
+                entity.Property(e => e.Titulo).HasColumnName("TITULO");
+                entity.Property(e => e.FechaPublicacion).HasColumnName("FECHA_PUBLICACION");
+                entity.Property(e => e.DoiUrl).HasColumnName("DOI_URL");
+                entity.Property(e => e.AreaConocimiento).HasColumnName("AREA_CONOCIMIENTO");
+                entity.Property(e => e.Observaciones).HasColumnName("OBSERVACIONES");
+                entity.Property(e => e.PdfProduccion).HasColumnName("PDF_PRODUCCION");
+            });
+        }
+
+    }
+
+}
---
+++ PromocionesDocente.Infraestructure/Data/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PromocionDocente.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using PromocionDocente.Domain.Entities; // Asegúrate de tener tus modelos allí
+
+namespace PromocionDocente.Infrastructure.Data
+{
+   
+}
