dca23eca9af0c88c8733e95213bfe94863a95c76
ArteMisa82 <anavero162@gmail.com>
2025-06-11 18:15:22 -0500

    Agregando formulario de docente

---
+++ PromocionDocente.Client/Models/Obras.cs
@@ -0,0 +1,22 @@
+﻿namespace PromocionDocente.Client.Models
+{
+
+        public class Obra
+        {
+            public int Id { get; set; }
+            public string Titulo { get; set; } = "";
+            public string Autor { get; set; } = "";
+            public string Tipo { get; set; } = "";
+            public DateTime FechaPublicacion { get; set; }
+            public string Estado { get; set; } = "";
+            public bool Seleccionado { get; set; }
+        }
+
+        public class Docente_Info
+        {
+            public string Nombre { get; set; } = "";
+            public string Cargo { get; set; } = "";
+            public string Avatar { get; set; } = "";
+        }
+    
+}
