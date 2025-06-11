58916d8e8046b04170bf1ab97ab6009779e2a4c0
Josue Guevara <el.jdragon@gmail.com>
2025-06-11 15:16:04 -0500

    Añadir componentes para uso
    COmponentes restantes para uso antes de creaciond e controladores

--- PromocionDocente.API/Program.cs
+++ PromocionDocente.API/Program.cs
@@ -20,8 +20,10 @@ builder.Services.AddDbContext<DACDbContext>(options =>
 builder.Services.AddDbContext<PromocionDocenteDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PROMOCION_DOCENTE")));
 
-// Inyecta el servicio de importación
+// Inyecta el servicio de importación de las Obras
 builder.Services.AddScoped<IObraImportService, ObraImportService>();
+// Inyecta servicio de Importacion de las evaluaciones
+builder.Services.AddScoped<IEvaluacionImportService, EvaluacionImportService>();
 
 var app = builder.Build();
 
---
+++ PromocionDocente.Application/Interfaces/IEvaluacionImportService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace PromocionDocente.Application.Interfaces
+{
+    public interface IEvaluacionImportService
+    {
+        /// <summary>
+        /// Importa evaluaciones desde la base de datos externa (DAC) hacia la base local (PROMOCION_DOCENTE).
+        /// </summary>
+        Task ImportarEvaluacionesDesdeDACAsync();
+    }
+}
