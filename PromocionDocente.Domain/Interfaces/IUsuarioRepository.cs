1ef0462d61d3596ec6c8636515e207e86bc6a060
Ismael <Jcuadrado1092@uta.edu.ec>
2025-06-11 23:36:44 -0500

    Se realizo el login

---
+++ PromocionDocente.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,9 @@
+﻿using PromocionDocente.Infrastructure.Data;
+
+namespace PromocionDocente.Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario?> LoginAsync(string correo, string contrasena);
+    }
+}
---
+++ PromocionDocente.Repositorio/Repository/Usuario_Repository.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PromocionDocente.Domain.Interfaces;
+using PromocionDocente.Infrastructure.Data;
+
+namespace Repository
+{
+    public class Usuario_Repository : IUsuarioRepository
+    {
+        private readonly DiticContext _context;
+        public Usuario_Repository(DiticContext context)
+        {
+            _context = context;
+        }
+
+
+        public Task<Usuario?> LoginAsync(string correo, string contrasena)
+        {
+            return _context.Usuarios
+            .FirstOrDefaultAsync(u => u.Correo == correo && u.Contrasena == contrasena);
+        }
+    }
+}
