48ebb7a8df54baced9827f7b138d54adb90cd576
Ismael <Jcuadrado1092@uta.edu.ec>
2025-06-23 21:41:16 -0500

    Se realizo la implementacion de metodo para el manejo de localStorage

---
+++ PromocionDocente.Client/Services/UserSessionService.cs
@@ -0,0 +1,37 @@
+﻿using Blazored.LocalStorage;
+using System.Threading.Tasks;
+
+namespace PromocionDocente.Client.Services
+{
+    public class UserSessionService
+    {
+        private readonly ILocalStorageService _localStorage;
+        public LoginResponse? Usuario { get; private set; }
+
+        public UserSessionService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public async Task CargarUsuarioAsync()
+        {
+            Usuario = await _localStorage.GetItemAsync<LoginResponse>("usuario");
+        }
+
+       
+        public async Task EliminarUsuarioAsync()
+        {
+          
+            Usuario = null;
+            await Task.CompletedTask;
+        }
+    }
+
+    public class LoginResponse
+    {
+        public string Cedula { get; set; } = string.Empty;
+        public string Nombre1 { get; set; } = string.Empty;
+        public string Apellido1 { get; set; } = string.Empty;
+        public string Rol { get; set; } = string.Empty;
+    }
+}
