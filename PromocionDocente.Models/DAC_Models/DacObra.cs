773b0f812127fb630baea20a69a7f613349b6d8b
Josue Guevara <el.jdragon@gmail.com>
2025-07-01 15:53:23 -0500

    Colo car los añadir en la vista funcionamiento correcto

--- PromocionDocente.Models/DAC_Models/DacObra.cs
+++ PromocionDocente.Models/DAC_Models/DacObra.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PromocionDocente.Models.DAC_Models;
 
+[Table("OBRAS", Schema = "dbo")]
 public partial class DacObra
 {
     public int IdObra { get; set; }
@@ -13,7 +13,7 @@ public partial class DacObra
 
     public string Titulo { get; set; } = null!;
 
-    public DateOnly FechaPublicacion { get; set; }
+    public DateTime FechaPublicacion { get; set; }  // Usa DateTime, no DateOnly
 
     public string? DoiUrl { get; set; }
 
